deeeb7845f208d325a2bedacca9799137f8acc29
Brian Davis <bdavis@procuredhealth.com>
2017-12-05 08:18:11 -0600

    Rearrange

--- join_test.py
+++ join_test.py
@@ -2,6 +2,55 @@ import time
 import psycopg2
 
 
+def main():
+    conn = psycopg2.connect(host='localhost', dbname='join_test', user='root')
+    conn.set_session(autocommit=True)
+    cursor = conn.cursor()
+
+    num_tables = 50
+    num_rows = 2560000
+
+    cursor.execute('BEGIN;')
+
+    drop_and_recreate_schema(cursor)
+    set_search_path(cursor)
+    create_tables(cursor, num_tables)
+    populate_tables(cursor, num_tables, num_rows, 10)
+    create_indexes_on_foreign_keys(cursor, num_tables)
+    analyze_tables(cursor, num_tables)
+
+    cursor.execute('COMMIT;')
+
+    print "\nTables:          {}".format(num_tables)
+    print "Rows per table:  {}\n".format(num_rows)
+    print "DB Size:         {}\n".format(get_db_size(cursor))
+
+    start_time = time.time()
+    cursor.execute(get_sql_statement(num_tables))
+    print "No WHERE clause: {}ms".format(int((time.time() - start_time) * 1000))
+
+    start_time = time.time()
+    cursor.execute(get_sql_statement(num_tables, 10))
+    print "   WHERE clause: {}ms\n".format(int((time.time() - start_time) * 1000))
+
+
+def drop_and_recreate_schema(cursor):
+    """
+    Drop all tables from any previous tests.  Important for measuring db size
+    """
+
+    cursor.execute("""
+        DROP SCHEMA IF EXISTS join_test CASCADE;
+        CREATE SCHEMA join_test;
+    """)
+
+
+def set_search_path(cursor):
+    cursor.execute("""
+        SET search_path TO join_test;
+    """)
+
+
 def create_tables(cursor, num_tables):
     cursor.execute("""
         DROP TABLE IF EXISTS table_1 CASCADE;
@@ -38,6 +87,20 @@ def populate_tables(cursor, num_tables, num_rows, mod):
             """.format(table_num=table_num, previous_table_num=table_num - 1))
 
 
+def create_indexes_on_foreign_keys(cursor, num_tables):
+    for table_num in xrange(2, num_tables + 1):
+        cursor.execute("""
+            CREATE INDEX ON table_{table_num} (table_{previous_table_num}_id);
+        """.format(table_num=table_num, previous_table_num=table_num - 1))
+
+
+def analyze_tables(cursor, num_tables):
+    for table_num in xrange(1, num_tables + 1):
+        cursor.execute("""
+            ANALYZE table_{table_num};
+        """.format(table_num=table_num, previous_table_num=table_num - 1))
+
+
 def get_sql_statement(num_tables, max_id=None):
     query = """
         SET search_path TO join_test;
@@ -64,37 +127,6 @@ def get_sql_statement(num_tables, max_id=None):
     return query + ';'
 
 
-def create_indexes_on_foreign_keys(cursor, num_tables):
-    for table_num in xrange(2, num_tables + 1):
-        cursor.execute("""
-            CREATE INDEX ON table_{table_num} (table_{previous_table_num}_id);
-        """.format(table_num=table_num, previous_table_num=table_num - 1))
-
-
-def analyze_tables(cursor, num_tables):
-    for table_num in xrange(1, num_tables + 1):
-        cursor.execute("""
-            ANALYZE table_{table_num};
-        """.format(table_num=table_num, previous_table_num=table_num - 1))
-
-
-def drop_and_recreate_schema(cursor):
-    """
-    Drop all tables from any previous tests.  Important for measuring db size
-    """
-
-    cursor.execute("""
-        DROP SCHEMA IF EXISTS join_test CASCADE;
-        CREATE SCHEMA join_test;
-    """)
-
-
-def set_search_path(cursor):
-    cursor.execute("""
-        SET search_path TO join_test;
-    """)
-
-
 def get_db_size(cursor):
     cursor.execute("""
         SELECT
@@ -106,41 +138,6 @@ def get_db_size(cursor):
     """)
 
     return cursor.fetchone()[0]
-
-
-def main():
-    conn = psycopg2.connect(host='localhost', dbname='join_test', user='bdavis')
-    conn.set_session(autocommit=True)
-    cursor = conn.cursor()
-
-    num_tables = 50
-    num_rows = 2560000
-
-
-
-    cursor.execute('BEGIN;')
-
-    drop_and_recreate_schema(cursor)
-    set_search_path(cursor)
-    create_tables(cursor, num_tables)
-    populate_tables(cursor, num_tables, num_rows, 10)
-    create_indexes_on_foreign_keys(cursor, num_tables)
-    analyze_tables(cursor, num_tables)
-
-    cursor.execute('COMMIT;')
-
-    print "\nTables:          {}".format(num_tables)
-    print "Rows per table:  {}\n".format(num_rows)
-
-    print "DB Size:         {}\n".format(get_db_size(cursor))
-
-    start_time = time.time()
-    cursor.execute(get_sql_statement(num_tables))
-    print "No WHERE clause: {}ms".format(int((time.time() - start_time) * 1000))
-
-    start_time = time.time()
-    cursor.execute(get_sql_statement(num_tables, 10))
-    print "   WHERE clause: {}ms\n".format(int((time.time() - start_time) * 1000))
 
 
 if __name__ == "__main__":
