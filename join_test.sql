219adefe7d81847d86bad58abd99509d375928fb
Brian Davis <the.brian.m.davis@gmail.com>
2017-12-26 19:15:08 -0600

    Do it all in one function / loop

--- join_test.sql
+++ join_test.sql
@@ -2,15 +2,20 @@
 \set num_rows 1000
 
 
-
 DROP FUNCTION IF EXISTS create_tables;
-CREATE FUNCTION create_tables(integer) RETURNS void AS $function_text$
+CREATE FUNCTION create_tables(integer, integer) RETURNS void AS $function_text$
 BEGIN
 
 DROP TABLE IF EXISTS table_1 CASCADE;
 CREATE TABLE table_1 (
     id serial primary key
 );
+
+INSERT INTO table_1 (id)
+SELECT
+    nextval('table_1_id_seq')
+FROM
+    generate_series(1, $2);
 
 
 FOR i IN 2..$1 LOOP
@@ -21,37 +26,7 @@ FOR i IN 2..$1 LOOP
             id serial primary key,
             table_%2$s_id integer references table_%2$s (id)
 	);
-    $$, i, i-1);
 
-END LOOP;
-END;
-$function_text$ LANGUAGE plpgsql;
-
-
-SELECT create_tables(:table_num);
-
-
-INSERT INTO table_1 (id)
-SELECT
-    nextval('table_1_id_seq')
-FROM
-    generate_series(1, :num_rows);
-
-
-
-DROP FUNCTION IF EXISTS populate_tables;
-CREATE FUNCTION populate_tables(integer, integer) RETURNS void AS $function_text$
-BEGIN
-
-INSERT INTO table_1 (id)
-SELECT
-    nextval('table_1_id_seq')
-FROM
-    generate_series(1, $2);
-
-
-FOR i IN 2..$1 LOOP
-    EXECUTE format($$
         INSERT INTO table_%1$s (table_%2$s_id)
         SELECT
             id
@@ -68,4 +43,5 @@ END;
 $function_text$ LANGUAGE plpgsql;
 
 
-SELECT populate_tables(:table_num, :num_rows);
+SELECT create_tables(:table_num, :num_rows);
+
