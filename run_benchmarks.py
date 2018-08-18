12a8ff87f51199c4ab0ebcb4049f50196d3a2cda
Brian Davis <the.brian.m.davis@gmail.com>
2018-08-18 14:06:37 -0500

    Break run_benchmarks.py into two files

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -1,65 +1,49 @@
+import argparse
 import subprocess
 
 
-"""
-export PGHOST="localhost"
-export PGDATABASE="join_test"
-export PGUSER="brian"
-"""
+def main():
+    parser = argparse.ArgumentParser(description='Run a benchmark')
+    parser.add_argument('--max-tables', type=int, required=True)
+    parser.add_argument('--max-max-rows', type=int, required=True)
+    parser.add_argument('--max-id', type=int, required=True)
+    parser.add_argument('--create-indexes', type=bool, required=True)
+
+    args = parser.parse_args()
+
+    benchmarks = create_benchmarks(args.max_tables, args.max_max_rows, args.max_id, args.create_indexes)
+    run_benchmarks(benchmarks)
 
 
-benchmark_descriptions = [
-    {
-        'max_tables': 50,
-        'max_max_rows': 100000,
-        'max_id': 10,
-        'create_indexes': False,
-    },
-    {
-        'max_tables': 50,
-        'max_max_rows': 100000,
-        'max_id': 10,
-        'create_indexes': True,
-    },
-    {
-        'max_tables': 200,
-        'max_max_rows': 1000,
-        'max_id': 10,
-        'create_indexes': False,
-    },
-    {
-        'max_tables': 200,
-        'max_max_rows': 100000,
-        'max_id': 10,
-        'create_indexes': True,
-    },
-]
-
-
-benchmarks = []
-
-
-for bd in benchmark_descriptions:
+def create_benchmarks(max_tables, max_max_rows, max_id, create_indexes):
+    benchmarks = []
     max_rows = 10
 
-    while max_rows <= bd['max_max_rows']:
+    while max_rows <= max_max_rows:
         benchmarks.append({
-            'max_tables': bd['max_tables'],
+            'max_tables': max_tables,
             'rows': max_rows,
-            'max_id':bd['max_id'],
-            'create_indexes': bd['create_indexes']
+            'max_id': max_id,
+            'create_indexes': create_indexes
         })
 
         max_rows = max_rows * 10
 
+    return benchmarks
 
-for benchmark in benchmarks:
-    max_tables = "max_tables={}".format(benchmark['max_tables'])
-    rows = "rows={}".format(benchmark['rows'])
-    max_id = "max_id={}".format(benchmark['max_id'])
-    create_indexes = "create_indexes={}".format(benchmark['create_indexes'])
 
-    subprocess.call(["psql", "-v", max_tables, "-v", rows, "-v", max_id, "-v", create_indexes, "-f", "benchmark.sql"])
+def run_benchmarks(benchmarks):
+    for benchmark in benchmarks:
+        max_tables = "max_tables={}".format(benchmark['max_tables'])
+        rows = "rows={}".format(benchmark['rows'])
+        max_id = "max_id={}".format(benchmark['max_id'])
+        create_indexes = "create_indexes={}".format(benchmark['create_indexes'])
 
-    command = "\copy benchmark_results TO /home/brian/angryjoin/benchmark_results/db.m4.large_max_tables_{}_rows_{}_max_id_{}_create_indexes_{}.csv DELIMITER ',' CSV HEADER;".format(benchmark['max_tables'], benchmark['rows'], benchmark['max_id'], benchmark['create_indexes'])
-    subprocess.call(["psql", "-c", command])
+        subprocess.call(["psql", "-v", max_tables, "-v", rows, "-v", max_id, "-v", create_indexes, "-f", "benchmark.sql"])
+
+        command = "\copy benchmark_results TO /home/brian/angryjoin/benchmark_results/db.m4.large_max_tables_{}_rows_{}_max_id_{}_create_indexes_{}.csv DELIMITER ',' CSV HEADER;".format(benchmark['max_tables'], benchmark['rows'], benchmark['max_id'], benchmark['create_indexes'])
+        subprocess.call(["psql", "-c", command])
+
+
+if __name__ == "__main__":
+        main()
