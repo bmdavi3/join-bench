07c14db90c4eb163d816821ee6313479a3a3c9bb
Brian Davis <the.brian.m.davis@gmail.com>
2018-08-19 12:35:59 -0500

    Create plotly charts as output

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -2,20 +2,68 @@ import argparse
 import json
 import subprocess
 
+from jinja2 import Template
+import plotly.graph_objs as go
+import psycopg2
+from psycopg2.extras import DictCursor
 
-"""
-How are we going to describe benchmarks?
 
-What can we specify?
+def install_benchmark_database_objects():
+    subprocess.call(["psql", "-f", "install_benchmark_database_objects.sql"])
 
-  - max-tables
-  - max-rows
-  - max-id
-  - whether we have indexes
-  - how many extra columns
-  - how many times to do each query
-  - output file for results
-"""
+
+def truncate_benchmark_results(cursor):
+    cursor.execute("""
+        TRUNCATE benchmark_results;
+    """)
+
+
+def render_html(figure, filename):
+    with open('plotly_html_template.jinja', 'r') as template_file:
+        template = Template(template_file.read())
+        with open(filename, 'w') as outfile:
+            outfile.write(template.render(figure=figure))
+
+
+def generate_plotly(cursor, title, filename):
+    cursor.execute("""
+        WITH foo AS (
+            SELECT
+                tables,
+                rows,
+                EXTRACT(EPOCH FROM avg(duration)) AS duration
+            FROM
+                benchmark_results
+            GROUP BY
+                tables,
+                rows
+            ORDER BY
+                tables,
+                rows
+        )
+        SELECT
+            rows,
+            array_agg(tables order by tables) AS tables,
+            array_agg(duration order by tables) AS durations
+        FROM
+            foo
+        GROUP BY
+            rows
+        ORDER BY
+            rows
+    """)
+
+    data = []
+
+    for row in cursor.fetchall():
+        trace = go.Scatter(x=row['tables'], y=row['durations'], name='{} rows'.format(row['rows']), legendgroup='whatever')
+
+        data.extend([trace])
+
+    layout = go.Layout(title=title, xaxis=dict(title='Tables'), yaxis=dict(title='Seconds', type='log', autorange=True))
+    figure = go.Figure(data=data, layout=layout)
+
+    render_html(json.dumps(figure.to_plotly_json()), filename)
 
 
 def main():
@@ -27,9 +75,17 @@ def main():
     with open(args.filename) as f:
         benchmark_descriptions = json.load(f)
 
+    install_benchmark_database_objects()
+
+    connection = psycopg2.connect("", cursor_factory=DictCursor)
+    connection.autocommit = True
+    cursor = connection.cursor()
+
     for bd in benchmark_descriptions:
+        truncate_benchmark_results(cursor)
         benchmarks = create_benchmarks(bd['max-tables'], bd['max-rows'], bd['max-id'], bd['extra-columns'], bd['create-indexes'], bd['output-filename'])
         run_benchmarks(benchmarks)
+        generate_plotly(cursor, bd['plot-title'], bd['plot-filename'])
 
 
 def create_benchmarks(max_tables, max_rows, max_id, extra_columns, create_indexes, output_filename):
